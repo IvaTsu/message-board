c02311d40a0744961dc4f82d2d3da34502f4c2e4
ivatsu <ivan.tsud@gmail.com>
2018-01-12 13:22:08 +0200

    cleaning up the project files

---
+++ src/Firebase.js
@@ -0,0 +1,14 @@
+ import * as firebase from 'firebase';
+
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyBPzNEzafhbyYxdwjrKAPOnv-rAnL5rDBE",
+    authDomain: "message-board-8e6bc.firebaseapp.com",
+    databaseURL: "https://message-board-8e6bc.firebaseio.com",
+    projectId: "message-board-8e6bc",
+    storageBucket: "message-board-8e6bc.appspot.com",
+    messagingSenderId: "773453152586"
+  };
+  firebase.initializeApp(config);
+
+  export const database = firebase.database().ref('posts/');
