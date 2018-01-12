a2cee18ed9a664b9a89aadba814c8d908a0c2a23
ivatsu <ivan.tsud@gmail.com>
2018-01-12 14:04:45 +0200

    added view level

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import _ from 'lodash';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: {
+        'key0': {
+          title: 'Title',
+          body: 'Body'
+        }
+      }
+    }
+  };
+
+  renderPosts() {
+    return _.map(this.state.posts, (post, key) => {
+      return (
+        <div key={key}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      )
+    })
+  };
+
   render() {
     return (
       <div>
-
+        {this.renderPosts()}
       </div>
     );
   }
