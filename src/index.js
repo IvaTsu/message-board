dd2e678c9d6952cc0636950dc80c6c1789645daf
ivatsu <ivan.tsud@gmail.com>
2018-01-15 13:42:38 +0200

    added react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import reducers from './Reducers/index';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
